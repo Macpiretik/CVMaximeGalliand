fb112b5b833ef0b1216f07023a6a9750213e5d5c
Macpiretik <macpiretik@gmail.com>
2023-06-14 17:04:40 +0200

    problème de header et sidebar lors de la modal réglés

--- Styles/script.js
+++ Styles/script.js
@@ -1,8 +1,12 @@
 const modalContainer = document.querySelector(".modal-container");
 const modalTriggers = document.querySelectorAll(".modal-trigger");
+const header = document.querySelector(".header");
+const sidebar = document.querySelector(".sidebar");
 
 modalTriggers.forEach(trigger => trigger.addEventListener("click", toggleModal));
 
 function toggleModal(){
     modalContainer.classList.toggle("active");
+    header.classList.toggle("active");
+    sidebar.classList.toggle("active");
 }
